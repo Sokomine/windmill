a534e30c96110ea010441875526552f1bdad0718
Sokomine <wegwerf@anarres.dyndns.org>
2013-10-16 19:05:00 +0200

    added README and counterclockwise rotating rotors

--- init.lua
+++ init.lua
@@ -1,11 +1,37 @@
 
 minetest.register_node("windmill:windmill", {
-	description = "Windmill rotors",
-	drawtype = "signlike", --"signlike", --"signlike",
+	description = "Windmill rotors (clockwise)",
+	drawtype = "signlike", 
         visual_scale = 6.0,
-	--tiles = {"default_torch_on_floor.png", "default_torch_on_ceiling.png", "default_torch.png"},
 	tiles = {
 		{name="windmill.png", animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=1.0}},
+	},
+--	inventory_image = "default_torch_on_floor.png",
+--	wield_image = "default_torch_on_floor.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	light_source = 1, -- reflecting a bit of light might be expected
+	selection_box = {
+		type = "wallmounted",
+--		wall_top =    {-0.1,  0.5-2.6, -0.1, 0.1, 0.5, 0.1},
+--		wall_bottom = {-0.1, -0.5, -0.1, 0.1, -0.5+2.6, 0.1},
+		wall_side   = {-0.4, -2.5, -2.5, -0.2, 2.5, 2.5},
+	},
+	groups = {choppy=2,dig_immediate=3,attached_node=1},
+	legacy_wallmounted = true,
+
+})
+
+
+-- this one rotates in the opposite direction than the first one
+minetest.register_node("windmill:windmill_reverse", {
+	description = "Windmill rotors (counterclockwise)",
+	drawtype = "signlike",
+        visual_scale = 6.0,
+	tiles = {
+		{name="windmill_reverse.png", animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=1.0}},
 	},
 --	inventory_image = "default_torch_on_floor.png",
 --	wield_image = "default_torch_on_floor.png",
@@ -46,3 +72,29 @@ minetest.register_node("windmill:axis", {
 	},
 })
 
+
+minetest.register_craft({
+        output = "windmill:windmill",
+        recipe = {
+                {"default:steel_ingot", "",                    "default:steel_ingot", },
+                {"",                    "default:stick",       "",                    },
+                {"default:steel_ingot", "",                    "default:steel_ingot"  },
+        }
+})
+
+minetest.register_craft({
+        output = "windmill:windmill",
+        recipe = {{ "windmill:windmill_reverse" }},
+})
+
+minetest.register_craft({
+        output = "windmill:windmill_reverse",
+        recipe = {{ "windmill:windmill" }},
+})
+
+minetest.register_craft({
+        output = "windmill:axis",
+        recipe = {
+                {"default:steel_ingot", "default:stick",       "default:steel_ingot" },
+        }
+})
